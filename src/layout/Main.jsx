8b98cea75909384bfb5bfef3b64dacb8d3505164
Azamat <azamt14@gmail.com>
2021-12-13 19:49:05 +0300

    Main.jsx refactor on hooks

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,49 +1,54 @@
 import Movies from "../components/Movies"
-import React from "react" 
+import React, { useEffect, useState } from "react" 
 import Preloader from '../components/Preloader'
 import Search from "../components/Search"
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
-class Main extends React.Component {
-    state = {
-        movies: [],
-        loading: true
-    }
+const Main = () => {
 
-    componentDidMount = () => {
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
+    
+
+    useEffect(() => {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
         .then(responce => responce.json())
-        .then(data => this.setState({movies: data.Search, loading: false}))
+        .then(data => {
+            setMovies(data.Search) 
+            setLoading(false)
+        })
         .catch((err) => {
             console.error(err)
-            this.setState({loading: false})
+            setLoading(false)
         })
-    }
+    }, [])
+
  
-    updateMovies = (s, type) => {
-        this.setState({loading: true})
+    const updateMovies = (s, type) => {
+        setLoading(false)
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${s}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(responce => responce.json())
-        .then(data => this.setState({movies: data.Search, loading: false}))
+        .then(data => {
+            setMovies(data.Search) 
+            setLoading(false)
+        })
         .catch((err) => {
             console.error(err)
-            this.setState({loading: false})
+            setLoading(false)
         })
     }
 
-    render() {
-        return(
-            <main className='container content'>
-                <Search updateMovies={this.updateMovies} />
+    return(
+        <main className='container content'>
+            <Search updateMovies={updateMovies} />
 
-                {this.state.loading ? <Preloader />
-                : <Movies movies={this.state.movies} />}
-                
-            </main>
+            {loading ? <Preloader />
+            : <Movies movies={movies} />}
             
-        )
-    }
+        </main>
+        
+    )
 }
 
 export default Main
